a8cabfccb57b06e68d960bf2d6e5f31f2911f7f5
Jose Aguilera <60164793+JAguilera3515@users.noreply.github.com>
2020-04-21 18:00:04 -0400

    Create proifile.py

---
+++ proifile.py
@@ -0,0 +1,25 @@
+import geni.portal as portal
+# Import the ProtoGENI library.
+import geni.rspec.pg as pg
+
+# Create a portal context.
+pc = portal.Context()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+
+# Add a raw PC to the request.
+node = request.RawPC("node")
+
+# Install and execute a script that is contained in the repository.
+node.addService(pg.Execute(shell="/bin/sh", command="sudo apt install docker.io"))
+
+node.addService(pg.Execute(shell="/bin/sh", command="sudo bash /local/repository/install_docker.sh"))
+node.addService(pg.Execute(shell="/bin/sh", command="cd /local/repository"))
+#node.addService(pg.Execute(shell="/bin/sh", command="docker-compose up"))
+
+
+node.addService(pg.Execute(shell="/bin/sh", command="/local/repository/docker-compose.yml"))
+
+# Print the RSpec to the enclosing page.
+pc.printRequestRSpec(request)
